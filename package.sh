f271c78c97c248feca52be6cc9b168bf47952470
Philip O'Toole <philip.otoole@yahoo.com>
2015-01-21 22:12:14 -0800

    Significant additions

--- package.sh
+++ package.sh
@@ -1,29 +1,74 @@
 #!/bin/bash
 
-# Check that GOPATH is set.
-# Check tree is clean.
-# Build binaries.
-# Check expected binaries exist.
-# Check all error codes.
-# Clean up on exit -- bad or otherwise.
-# Let user know at each step, confirm if necessary.
+INSTALL_ROOT_DIR=/opt/influxdb
 
-INFLUXDB_DIR=/opt/influxdb
+TMP_WORK_DIR=`mktemp -d`
+POST_INSTALL_PATH=`mktemp`
+ARCH=`uname -i`
 
-TMPDIR=`mktemp -d`
-TMP_POSTINSTALL=`mktemp`
+# usage prints simple usage information.
+usage() {
+    echo -e "$0 [<version>] [-h]\n"
+    cleanup_exit $1
+}
 
-VERSION=`$GOPATH/bin/influxd version 2>&1 | cut -d ' ' -f 2`
-if [ -z "$VERSION" ]; then
-    exit 1
-fi
+# cleanup_exit removes all resources created during the process and exits with
+# the supplied returned code.
+cleanup_exit() {
+    rm -r $TMP_WORK_DIR
+    rm $POST_INSTALL_PATH
+    exit $1
+}
 
-mkdir -p $TMPDIR/$INFLUXDB_DIR/versions/$VERSION
-cp $GOPATH/bin/* $TMPDIR/$INFLUXDB_DIR/versions/$VERSION
+# check_gopath sanity checks the value of the GOPATH env variable.
+check_gopath() {
+    [ -z "$GOPATH" ] && echo "GOPATH is not set." && cleanup_exit 1
+    [ ! -d "$GOPATH" ] && echo "GOPATH is not a directory." && cleanup_exit 1
+    echo "GOPATH ($GOPATH) looks sane."
+}
 
-cat  <<EOF >$TMP_POSTINSTALL
-rm $INFLUXDB_DIR/influxd
-ln -s  $INFLUXDB_DIR/versions/$VERSION/influxd $INFLUXDB_DIR/influxd
+# check_clean_tree ensures that no source file is locally modified.
+check_clean_tree() {
+    modified=$(git ls-files --modified | wc -l)
+    if [ $modified -ne 0 ]; then
+        echo "The source tree is not clean -- aborting."
+        cleanup_exit 1
+    fi
+    echo "Git tree is clean."
+}
+
+# update_tree ensures the tree is in-sync with the repo.
+update_tree() {
+    git pull origin master
+    if [ $? -ne 0 ]; then
+        echo "Failed to pull latest code -- aborting."
+        cleanup_exit 1
+    fi
+    git fetch --tags
+    if [ $? -ne 0 ]; then
+        echo "Failed to fetch tags -- aborting."
+        cleanup_exit 1
+    fi
+    echo "Git tree updated successfully."
+}
+
+# do_build builds the code.
+do_build() {
+    go build ./...
+    if [ $? -ne 0 ]; then
+        echo "Build failed, unable to create package -- aborting"
+        cleanup_exit 1
+    fi
+    echo "Build completed successfully."
+}
+
+# generate_postinstall_script creates the post-install script for the
+# package. It must be passed the version.
+generate_postinstall_script() {
+    version=$1
+    cat  <<EOF >$POST_INSTALL_PATH
+rm $INSTALL_ROOT_DIR/influxd
+ln -s  $INSTALL_ROOT_DIR/versions/$version/influxd $INSTALL_ROOT_DIR/influxd
 
 if ! id influxdb >/dev/null 2>&1; then
         useradd --system -U -M influxdb
@@ -31,7 +76,57 @@ fi
 chown -R -L influxdb:influxdb $INFLUXDB_DIR
 chmod -R a+rX $INFLUXDB_DIR
 EOF
+    echo "Post-install script created successfully."
+}
 
-fpm -s dir -t rpm --after-install $TMP_POSTINSTALL -n influxdb -v $VERSION $TMPDIR
-fpm -s dir -t deb -a i686 --after-install $TMP_POSTINSTALL -n influxdb -v -v $VERSION $TMPDIR
+# Start the packaging process.
 
+if [ $# -ne 1 ]; then
+    usage 1
+elif [ $1 == "-h" ]; then
+    usage 0
+else
+    VERSION=$1
+fi
+
+check_go_path
+check_clean_tree
+update_tree
+do_build
+
+mkdir -p $TMP_WORK_DIR/$INSTALL_ROOT_DIR/versions/$VERSION
+if [ $? -ne 0 ]; then
+    echo "Failed to create temporary work directory for packaging -- aborting."
+    cleanup_exit 1
+fi
+echo "Temporary work directory created in $TMP_WORK_DIR"
+
+cp $GOPATH/bin/* $TMP_WORK_DIR/$INSTALL_ROOT_DIR/versions/$VERSION
+if [ $? -ne 0 ]; then
+    echo "Failed to copy binaries to packaging directory -- aborting."
+    cleanup_exit 1
+fi
+echo "Binaries in $GOPATH/bin copied to $TMP_WORK_DIR/$INSTALL_ROOT_DIR/versions/$VERSION"
+
+generate_postinstall_script $VERSION
+
+echo -n "Commence creation of $ARCH packages, version $VERSION? [Y/n] "
+read response
+response=`echo $response | tr 'A-Z' 'a-z'`
+if [ "x$response" == "xn" ]; then
+    echo "Packaging aborted."
+    cleanup_exit 1
+fi
+
+
+echo fpm -s dir -t rpm --after-install $TMP_POSTINSTALL -n influxdb -v $VERSION $TMP_WORK_DIR
+if [ $? -ne 0 ]; then
+    echo "Failed to create RPM package -- aborting"
+    cleanup_exit 1
+fi
+
+echo fpm -s dir -t deb -a i686 --after-install $TMP_POSTINSTALL -n influxdb -v -v $VERSION $TMP_WORK_DIR
+if [ $? -ne 0 ]; then
+    echo "Failed to create Debian package -- aborting"
+    cleanup_exit 1
+fi
